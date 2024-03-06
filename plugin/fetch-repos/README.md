ae0ce41dbcc7349bedd3870a039eb018bcaa19c0
eyea <deyesea@gmail.com>
2024-03-06 15:16:39 +0800

    feat: 优化fetch-repos plugin 必填项，和说明文档

--- plugin/fetch-repos/README.md
+++ plugin/fetch-repos/README.md
@@ -3,7 +3,7 @@
 批量获取远程仓库信息，支持配置 groupId, projectid，以及黑白名单
 
 ## 环境
-本插件基于 `nx` 开发，请在对应初始化好的 `nx-workspace` 中使用
+本插件基于 `nx` 开发，请在对应初始化好的 `your-app` 中使用
 
 ## 安装
 
@@ -19,7 +19,7 @@ pnpm i @afuteam-nx/plugin-fetch-repos -D
     "executor": "@afuteam-nx/plugin-fetch-repos:run",
     "options": {
       "accessToken": "获取代码仓库的accessToken",
-      "resultPath": "结果输出的位置",
+      "resultPath": "结果输出的位置 文件名 allProjects.json ",
       "gitLabBaseUrl": "git仓库地址，比如 https://gitlab.com",
       "rootGroupId": "仓库group类型的id 比如 4203",
       "ignoreGroupIds": "排除的group类型的id [1424, 12212]",
@@ -35,5 +35,69 @@ pnpm i @afuteam-nx/plugin-fetch-repos -D
 ## 使用
 ```bash
 nx run your-app:fetch-repos
+```
+
+## 输出数据结构
+```json
+// allProjects.json
+[
+  {
+    "id": 28612,
+    "description": "@afuteam-nx 系列插件",
+    "name": "plugins",
+    "name_with_namespace": "",
+    "path": "",
+    "path_with_namespace": "",
+    "created_at": "2023-11-21T15:23:21.637+08:00",
+    "default_branch": "master",
+    "tag_list": [],
+    "ssh_url_to_repo": "",
+    "http_url_to_repo": "",
+    "web_url": "",
+    "avatar_url": null,
+    "star_count": 0,
+    "forks_count": 0,
+    "last_activity_at": "2024-03-05T20:49:03.274+08:00",
+    "_links": {
+      "self": "http://gitlab.com",
+      "issues": "http://gitlab.com/issues",
+      "merge_requests": "http://gitlab.com/merge_requests",
+      "repo_branches": "http://gitlab.com/repository/branches",
+      "labels": "http://gitlab.com/labels",
+      "events": "http://gitlab.com/events",
+      "members": "http://gitlab.com/members"
+    },
+    "archived": false,
+    "visibility": "private",
+    "resolve_outdated_diff_discussions": false,
+    "container_registry_enabled": true,
+    "issues_enabled": true,
+    "merge_requests_enabled": true,
+    "wiki_enabled": true,
+    "jobs_enabled": true,
+    "snippets_enabled": true,
+    "shared_runners_enabled": false,
+    "lfs_enabled": true,
+    "creator_id": 943,
+    "namespace": {
+      "id": 4957,
+      "name": "",
+      "path": "",
+      "kind": "group",
+      "full_path": "",
+      "parent_id": 4203
+    },
+    "import_status": "none",
+    "open_issues_count": 0,
+    "public_jobs": true,
+    "ci_config_path": null,
+    "shared_with_groups": [],
+    "only_allow_merge_if_pipeline_succeeds": false,
+    "request_access_enabled": false,
+    "only_allow_merge_if_all_discussions_are_resolved": false,
+    "printing_merge_request_link_enabled": true
+  },
+  // ... 其他项目
+]
 ```
 
--- plugin/fetch-repos/src/executors/run/executor.ts
+++ plugin/fetch-repos/src/executors/run/executor.ts
@@ -1,7 +1,6 @@
 import { RunExecutorSchema } from './schema';
-
-import axios from "axios";
-import Tools from "./utils/index.js"
+import axios from 'axios';
+import Tools from './utils/index.js';
 
 interface Project {
   id: number;
@@ -21,159 +20,164 @@ export default async function runExecutor(options: RunExecutorSchema) {
     ignoreGroupIds,
     ignoreProjectsIds,
     addRepoIds,
-    per_page = 100
-  } = options
+    per_page = 100,
+  } = options;
 
-  if(!accessToken || !resultPath || !gitLabBaseUrl || !rootGroupId) {
-    console.error("\n请仔细校验配置的参数\n")
-    return false
-  }
-
-  // 定义函数
-// 获取指定 Group ID 下的所有 Projects
-async function getProjects(groupId: number, page = 1): Promise<Project[]> {
-
-  try {
-    const response = await axios.get(
-      `${gitLabBaseUrl}/api/v4/groups/${groupId}/projects?per_page=${per_page}&page=${page}`,
-      {
-        headers: {
-          "PRIVATE-TOKEN": accessToken,
-        },
-      }
-    );
-
-    const status = await response.status;
-
-    if (status === 200) {
-      const projects = await response.data as Project[];
-      if (projects.length === per_page) {
-        return [...projects, ...(await getProjects(groupId, page + 1))];
-      }
-      return projects;
-    }
-    return [];
-  } catch (error) {
-    console.error("Error occurred while fetching projects:", error.message);
-    throw error;
-  }
-
-}
-
-// 获取指定 Group ID 下的所有子 Group
-async function getSubGroups(groupId: number, page = 1): Promise<any[]> {
-  try {
-    const response = await axios.get(
-      `${gitLabBaseUrl}/api/v4/groups/${groupId}/subgroups?per_page=${per_page}&page=${page}`,
-      {
-        headers: {
-          "PRIVATE-TOKEN": accessToken,
-        },
-      }
-    );
-
-    const status = await response.status;
-    if (status === 200) {
-      const subGroups = await response.data as SubGroup[];
-      // 排除 ignoreGroupIds
-      const afterFilterSubGroups = subGroups.filter(
-        ({ id }) => !ignoreGroupIds.includes(id)
+  // 获取指定 Group ID 下的所有 Projects
+  async function getProjects(groupId: number, page = 1): Promise<Project[]> {
+    try {
+      const response = await axios.get(
+        `${gitLabBaseUrl}/api/v4/groups/${groupId}/projects?per_page=${per_page}&page=${page}`,
+        {
+          headers: {
+            'PRIVATE-TOKEN': accessToken,
+          },
+        }
       );
-      if (subGroups.length === per_page) {
-        return [
-          ...afterFilterSubGroups,
-          ...(await getSubGroups(groupId, page + 1)),
-        ];
+
+      const status = await response.status;
+
+      if (status === 200) {
+        const projects = (await response.data) as Project[];
+        if (projects.length === per_page) {
+          return [...projects, ...(await getProjects(groupId, page + 1))];
+        }
+        return projects;
       }
-      return afterFilterSubGroups;
+      return [];
+    } catch (error) {
+      console.error('Error occurred while fetching projects:', error.message);
+      throw error;
     }
-
-    return [];
-  } catch (error) {
-    console.error("Error occurred while fetching subgroups:", error.message);
-    throw error;
   }
-}
 
-// 递归获取所有子 Group 下的 Projects，并提取所需字段
-async function getAllSubGroupProjects(groupId: number): Promise<Project[]> {
-  try {
-    const subProjects = await getProjects(groupId);
-    const subGroups = await getSubGroups(groupId);
+  // 获取指定 Group ID 下的所有子 Group
+  async function getSubGroups(groupId: number, page = 1): Promise<any[]> {
+    try {
+      const response = await axios.get(
+        `${gitLabBaseUrl}/api/v4/groups/${groupId}/subgroups?per_page=${per_page}&page=${page}`,
+        {
+          headers: {
+            'PRIVATE-TOKEN': accessToken,
+          },
+        }
+      );
 
-    // 使用 Promise.all 并行地获取每个子组的项目
-    const subGroupProjectsPromises = subGroups.map(group => getAllSubGroupProjects(group.id));
-    const subGroupProjectsArrays = await Promise.all(subGroupProjectsPromises);
+      const status = await response.status;
+      if (status === 200) {
+        const subGroups = (await response.data) as SubGroup[];
+        // 排除 ignoreGroupIds
+        const afterFilterSubGroups = subGroups.filter(
+          ({ id }) => !ignoreGroupIds.includes(id)
+        );
+        if (subGroups.length === per_page) {
+          return [
+            ...afterFilterSubGroups,
+            ...(await getSubGroups(groupId, page + 1)),
+          ];
+        }
+        return afterFilterSubGroups;
+      }
 
-    // 将所有子组的项目合并成一个数组
-    const allSubGroupProjects = [].concat(...subGroupProjectsArrays);
-
-    // 合并本组和所有子组的项目
-    return [...subProjects, ...allSubGroupProjects];
-  } catch (error) {
-    console.error("Error occurred while fetching projects:", error.message);
-    throw error;
+      return [];
+    } catch (error) {
+      console.error('Error occurred while fetching subgroups:', error.message);
+      throw error;
+    }
   }
-}
 
+  // 递归获取所有子 Group 下的 Projects，并提取所需字段
+  async function getAllSubGroupProjects(groupId: number): Promise<Project[]> {
+    try {
+      const subProjects = await getProjects(groupId);
+      const subGroups = await getSubGroups(groupId);
 
-// 获取指定 projectIds 的信息
-async function getProjectsByIds(ids: number[]): Promise<Project[]> {
-  if (!ids || ids.length === 0) {
-    return [];
+      // 使用 Promise.all 并行地获取每个子组的项目
+      const subGroupProjectsPromises = subGroups.map((group) =>
+        getAllSubGroupProjects(group.id)
+      );
+      const subGroupProjectsArrays = await Promise.all(
+        subGroupProjectsPromises
+      );
+
+      // 将所有子组的项目合并成一个数组
+      const allSubGroupProjects = [].concat(...subGroupProjectsArrays);
+
+      // 合并本组和所有子组的项目
+      return [...subProjects, ...allSubGroupProjects];
+    } catch (error) {
+      console.error('Error occurred while fetching projects:', error.message);
+      throw error;
+    }
   }
-  try {
-    const projects = await Promise.all(ids.map(repo_id =>
-      axios.get(`${gitLabBaseUrl}/api/v4/projects/${repo_id}`, { headers: { "PRIVATE-TOKEN": accessToken } })
-        .then(response => response.status === 200 ? response.data : null)
-        .catch(err => {
-          console.error(`Error occurred while fetching project ${repo_id}:`, err.message);
-          return null; // 如果请求失败，返回null
-        })
-    ));
 
-    return projects.filter((project): project is Project => project !== null); // 过滤掉错误的请求结果
-  } catch (error) {
-    console.error("Unexpected error occurred while fetching projects:", error.message);
-    throw error;
+  // 获取指定 projectIds 的信息
+  async function getProjectsByIds(ids: number[]): Promise<Project[]> {
+    if (!ids || ids.length === 0) {
+      return [];
+    }
+    try {
+      const projects = await Promise.all(
+        ids.map((repo_id) =>
+          axios
+            .get(`${gitLabBaseUrl}/api/v4/projects/${repo_id}`, {
+              headers: { 'PRIVATE-TOKEN': accessToken },
+            })
+            .then((response) =>
+              response.status === 200 ? response.data : null
+            )
+            .catch((err) => {
+              console.error(
+                `Error occurred while fetching project ${repo_id}:`,
+                err.message
+              );
+              return null; // 如果请求失败，返回null
+            })
+        )
+      );
+
+      return projects.filter((project): project is Project => project !== null); // 过滤掉错误的请求结果
+    } catch (error) {
+      console.error(
+        'Unexpected error occurred while fetching projects:',
+        error.message
+      );
+      throw error;
+    }
   }
-}
 
-const main = async () => {
-  try {
-    const rootGroupIdProjectsInfos = await getAllSubGroupProjects(rootGroupId);
-    const addProjectInfos = await getProjectsByIds(addRepoIds);
+  const main = async () => {
+    try {
+      const rootGroupIdProjectsInfos = await getAllSubGroupProjects(
+        rootGroupId
+      );
+      const addProjectInfos = await getProjectsByIds(addRepoIds);
 
-    const allProjectsMap = new Map<number, Project>();
-    const allProjectsTempArr: Project[] = [
-      ...rootGroupIdProjectsInfos,
-      ...addProjectInfos,
-    ];
+      const allProjectsMap = new Map<number, Project>();
+      const allProjectsTempArr: Project[] = [
+        ...rootGroupIdProjectsInfos,
+        ...addProjectInfos,
+      ];
 
-    allProjectsTempArr.forEach((item) => allProjectsMap.set(item.id, item));
-    const result = Array.from(allProjectsMap.values());
+      allProjectsTempArr.forEach((item) => allProjectsMap.set(item.id, item));
+      const result = Array.from(allProjectsMap.values());
 
-    const filteredResult = result.filter(
-      (item) => !ignoreProjectsIds.includes(item.id)
-    );
+      const filteredResult = result.filter(
+        (item) => !ignoreProjectsIds.includes(item.id)
+      );
 
-    console.log(
-      `\n${new Date()} \n当前使用的token是 ${accessToken}\n`
-    );
+      console.log(`\n${new Date()} \n当前使用的token是 ${accessToken}\n`);
 
-    Tools.writeRes2SomePath('allProjects.json', filteredResult, resultPath)
+      Tools.writeRes2SomePath('allProjects.json', filteredResult, resultPath);
 
-    return true
-  } catch (error) {
-    console.error("Error occurred:", error.message);
-    return false
-  }
-}
+      return true;
+    } catch (error) {
+      console.error('Error occurred:', error.message);
+      return false;
+    }
+  };
 
-
-  // 定义函数结束
-
-  const result = await main()
+  const result = await main();
   return {
     success: result,
   };
